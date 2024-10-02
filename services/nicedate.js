4d6cf0737ab775fdb74a40cf7451ed061b07dc44
Robert Ismo <RobertIsmo@protonmail.com>
2024-10-01 21:03:33 -0500

    updated for clarity

--- services/nicedate.js
+++
@@ -1,7 +0,0 @@
-export const currentDate = (date) => new Date(date ?? Date.now()).toLocaleDateString('en-US', {
-	weekday: 'long',
-	month: 'long',
-	day: 'numeric',
-	timeZoneName: 'longGeneric',
-	timeZone: 'America/Chicago'
-}).replace(' at', ',').replace('Time', 'Standard Time')
